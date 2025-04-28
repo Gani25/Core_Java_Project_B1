5a28325c1f4634150deacd75258fb6d2bfeb23de
Gani25 <memonabdulgani3d@gmail.com>
2025-04-28 20:06:57 +0530

    List Of Objects

---
+++ src/day18/ListEmp.java
@@ -0,0 +1,49 @@
+package day18;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import day18.entity.Employee;
+
+public class ListEmp {
+	
+	public static void main(String[] args) {
+		List<Employee> employees = new ArrayList<>();
+	
+		System.out.println(employees.isEmpty());
+		
+		Employee employee1 = new Employee(1010,"Rohit","Mehta",35000,30);
+		employees.add(employee1);
+		
+		employees.add(new Employee(1006, "Hrithik", "Gupta", 45000, 35));
+		employees.add(new Employee(1001, "Hrithik", "Sharma", 25000, 20));
+		employees.add(new Employee(1005, "Rishikesh", "Patil", 35000, 25));
+		employees.add(new Employee(1002, "Rohan", "Gupta", 45000, 40));
+		employees.add(new Employee(1008, "Pranjal", "Sharma", 22000, 23));
+		employees.add(new Employee(1003, "Rohit", "Upadhyay", 45000, 27));
+		employees.add(new Employee(1007, "Om", "Nibalkar", 45000, 20));
+		employees.add(new Employee(1004, "Abdul", "Memon", 22000, 25));
+		employees.add(new Employee(1009, "Rohit", "Patil", 45000, 33));
+		
+		System.out.println("----------------------------");
+		System.out.println("All Employees Are");
+		System.out.println("----------------------------");
+	
+		for(Employee emp:employees) {
+			System.out.println(emp);
+			
+			System.out.println();
+		}
+		
+		employees.sort(null);
+		
+		System.out.println("Sorting All Employees By Asc Order");
+		for(Employee emp:employees) {
+			System.out.println(emp);
+			
+			System.out.println();
+		}
+		
+	}
+
+}
---
+++ src/day18/entity/Employee.java
@@ -0,0 +1,75 @@
+package day18.entity;
+
+public class Employee {
+	
+	private int empId;
+	
+	private String firstName;
+	
+	private String lastName;
+
+	private double salary;
+	
+	private int age;
+
+	public Employee(int empId, String firstName, String lastName, double salary, int age) {
+		super();
+		this.empId = empId;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.salary = salary;
+		this.age = age;
+	}
+	
+	public Employee() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public int getEmpId() {
+		return empId;
+	}
+
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee {empId=" + empId + ", firstName=" + firstName + ", lastName=" + lastName + ", salary=" + salary
+				+ ", age=" + age + "}";
+	}
+	
+	
+}
